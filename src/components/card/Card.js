b6675f4b0c9c01b21105add2848d9765e16e44ae
yoruwitch <evelyn.fernandes4@gmail.com>
2024-10-15 14:45:56 -0300

    Initial commit

---
+++ src/components/card/Card.js
@@ -0,0 +1,19 @@
+import "./Card.css";
+
+const Card = () => {
+    return (
+        <>
+            <div className="card">
+                <h3>Teste</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tempore est, praesentium ullam quod nisi provident magnam
+                    nemo cum quibusdam soluta ipsum modi sit, a voluptates
+                    officia incidunt doloremque itaque ut?
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default Card;
